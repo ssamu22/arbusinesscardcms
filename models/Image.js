409af5f4e9d27c2c4eca7b3ba20cc79b145a685c
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2024-11-28 22:26:58 +0800

    remove award-item placeholder and comments in image.js

--- models/Image.js
+++ models/Image.js
@@ -36,43 +36,6 @@ class Image {
 
     return data; // Returning data if needed for further processing
   }
-
-  // static async getImages(imageIdsArray) {
-  //   // Fetch unique image data for distinct image IDs
-  //   const { data: imageData, error: imageError } = await supabase
-  //     .from("image")
-  //     .select("image_id, image_bucket, image_filename")
-  //     .in("image_id", [...new Set(imageIdsArray)]); // Fetch unique IDs
-
-  //   if (imageError) {
-  //     console.error("Error fetching images:", imageError);
-  //     return [];
-  //   }
-
-  //   // Create a mapping from image_id to its corresponding image object
-  //   const imageMap = imageData.reduce((map, image) => {
-  //     map[image.image_id] = image;
-  //     return map;
-  //   }, {});
-
-  //   // Map the original array of IDs to the corresponding images
-  //   const finalImageArray = imageIdsArray.map((id) => ({
-  //     image_bucket: imageMap[id].image_bucket,
-  //     image_filename: imageMap[id].image_filename,
-  //   }));
-
-  //   const fileArray = [];
-
-  //   finalImageArray.forEach((fileMap) => {
-  //     const imageBucket = fileMap.image_bucket.split("/");
-  //     const fname = `${imageBucket[1]}/${fileMap.image_filename}`;
-  //     fileArray.push(fname);
-  //   });
-
-  //   console.log(fileArray);
-
-  //   return finalImageArray; // Return the array with duplicated items
-  // }
 }
 
 module.exports = Image;
