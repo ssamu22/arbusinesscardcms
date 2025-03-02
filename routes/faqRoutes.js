587859e19c0df1723b5a2c71622f8a5a30065efd
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-03-02 21:47:30 +0800

    commit faqRoutes

--- routes/faqRoutes.js
+++ routes/faqRoutes.js
@@ -3,10 +3,5 @@ const router = express.Router();
 const controller = require("../controllers/faqController");
 router.route("/").get(controller.getFaqs);
 
-
 router.route("/updateAll").patch(controller.updateFaq);
-
-// router.get("/faq", faqController.getFaqs);
-// router.route("/faq").get(faqController.getFaqs).put(faqController.updateFaq);
-
 module.exports = router;
