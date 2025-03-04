11a3741ef7a6917883391980f79502d445356421
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-03-04 14:39:46 +0800

    removed commented controllers and routes

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,6 @@ const ensureAuthenticated = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/upload");
 const informationController = require("../controllers/informationController");
 const { uploadProfilePic } = require("../controllers/informationController");
-const scheduleController = require("../controllers/scheduleController");
-const achievementController = require("../controllers/achievementController");
-const contactController = require("../controllers/contactController");
-const organizationController = require("../controllers/organizationController");
-const { uploadImage } = require("../controllers/organizationController");
 
 // -- Overview Page Routes --
 // Route to get all departments
@@ -20,15 +15,5 @@ router.get("/details", ensureAuthenticated, informationController.getDetails);
 
 // Route to upload profile picture
 router.post("/information/image", upload.single("file"), uploadProfilePic);
-
-// -- Schedule page routes --
-// Route to get all schedules
-router.get("/schedule", scheduleController.getSchedule);
-
-// Route to update schedule
-router.post("/schedule/hours", scheduleController.updateSchedule);
-
-// Route to update consultation types
-router.post("/schedule/types", scheduleController.updateTypes);
 
 module.exports = router;
