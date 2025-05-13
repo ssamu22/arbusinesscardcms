1d590b3be990bdffb46149d8170c14bab1b154b6
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-05-14 05:49:47 +0800

    removed honorifics

--- public/scripts/bcard-section.js
+++ public/scripts/bcard-section.js
@@ -1229,9 +1229,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         console.log(
           "THE NAME:",
-          `${employee.honorifics ?? ""} ${employee.first_name} ${
-            employee.middle_name ?? ""
-          } ${employee.last_name}`
+          `${employee.first_name} ${employee.middle_name ?? ""} ${
+            employee.last_name
+          }`
         );
 
         targetNameText.text = `${employee.honorifics ?? ""} ${
